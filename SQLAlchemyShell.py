9c7c1272d06343b47f29461b557ed66e3fe37531
Adam Arutyunov <arutyunov.adam@yandex.ru>
2021-01-23 14:40:07 +0000

    Login generator and Alembic

--- SQLAlchemyShell.py
+++ SQLAlchemyShell.py
@@ -5,10 +5,11 @@ from data.models.labour import *
 from data.models.test import *
 from data.models.answer import *
 from Constants import *
+from lib.miscellaneous import *
 
 db_session.global_init(DATABASE_URI)
 session = db_session.create_session()
 
-# Файл, если нужно быстро что-то поменять в БД
+
 
 session.commit()
---
+++ lib/miscellaneous.py
@@ -0,0 +1,53 @@
+from random import choice, randint
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def fill(string, n):
+    while len(string) < n:
+        string = choice(alphabet).upper() + string
+
+    return string
+        
+
+def encode_login(school: int, form: int, littera: str, number: int) -> str:
+    school = school % 10000
+    form = form % 100
+    littera = littera[0].lower()
+    number = number % 100
+
+    last_number = number % 10
+    school_str = ''.join(list(map(lambda x: alphabet[int(x) + last_number], str(school))))
+    school_str = fill(school_str, 4)
+
+    form_str = ''.join(list(map(lambda x: alphabet[int(x)], str(form + last_number))))
+    form_str = fill(form_str, 2)
+
+    littera_str = alphabet[-alphabet.index(littera) - 1]
+
+    number_str = ''.join(list(map(lambda x: alphabet[int(x)], str(number))))
+    number_str = fill(number_str, 2)
+
+    additional_str = ''
+
+    for i in range(3):
+        additional_char = choice(alphabet)
+        if randint(0, 1) == 1:
+            additional_char = additional_char.upper()
+
+        additional_str += additional_char
+
+
+    encoded_login = school_str + form_str + littera_str + number_str + additional_str
+
+    return encoded_login
+
+
+def decode_login(login: str):
+    login = login[:-3]
+    school = login[:4]
+    form = login[4:6]
+    littera = login[6:7]
+    number = login[7:9]
+
+    return None
